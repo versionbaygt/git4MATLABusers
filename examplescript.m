d33070f0dee4af0723044d46b88b6587605c16d7
Gareth Thomas <gareth@versionbay.com>
2022-11-10 14:52:46 +0100

    fix: followed code analyzer recommendations

--- examplescript.m
+++ examplescript.m
@@ -1,8 +1,8 @@
 % Solve the quadratic equation
 
 a = 10;
-b = 3
-c = 1
+b = 3;
+c = 1;
 
-x1 = (-b - sqrt(b*b - 4*a*a))/(2*a)
-x2 = (-b + sqrt(b*b - 4*a*a))/(2*a)
+x1 = (-b - sqrt(b*b - 4*a*a))/(2*a);
+x2 = (-b + sqrt(b*b - 4*a*a))/(2*a);
